58298d09b6d911866f9ae36701420d62da58aa1a
Frank Yournet <yournetf@bxscience.edu>
2025-01-30 14:44:26 -0800

    Learning the basics.

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
-name = input("What is your name?")
-
+##Asks the user for name then says hello
+name = input("What is your name?\n")
 print("Hello " + name)
 
+##Asks the user for their birth year then calculates their age
+birthYear = input("Enter your birth year\n")
+age = 2025 - int(birthYear)
+print(name + " is " + str(age) + " years old!\n")
 
+##Gets the sum of two inputs
+first = input("Enter the first number here: \n")
+second = input("Enter the second number here: \n")
+sum = int(first) + int(second)
+
+print("The sum of your inputs is: \n" + str(sum))
